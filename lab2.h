1be9f1a2924712d32efd427d6ecf89c36e8906e4
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2022-12-16 23:56:09 -0500

    Update lab2.h

--- lab2.h
+++ lab2.h
@@ -15,8 +15,7 @@ typedef struct {
     int ending_col;
 } param_struct;
 
-//int** sudoku_board; // this board is shared by the threads
-//int* worker_validation; // this will hold the result from each thread
+ // this will hold the result from each thread
 
 /**
  * is_board_valid - Checks if the Sudoko board is valid or not.  Returns a 0 
